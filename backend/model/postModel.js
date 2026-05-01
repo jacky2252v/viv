c8afba4b4c0c1729678582e62354d43f1ba3ac6a
jacky2252v <vivekbaraiya000@gmail.com>
2026-05-01 17:36:51 +0530

    feat: initialize Post and User Mongoose models

--- backend/model/postModel.js
+++ backend/model/postModel.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 // models/User.js
 const postSchema = new mongoose.Schema({
   //   id: { type: Number, required: true },
-  title: { type: String, unique: true },
+  title: { type: String },
   description: { type: String },
 });
 
--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -4,7 +4,7 @@ const { default: mongoose } = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: { type: String, required: false },
   email: { type: String, unique: true },
-  password: { type: String, unique: true },
+  password: { type: String },
 });
 
 module.exports = mongoose.model('User', userSchema);
